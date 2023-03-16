f0a759e62d7ccb6b5c04fbbd337a4b4548c7ec22
a-tornyie <a.tornyie@gmail.com>
2023-03-16 06:17:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Use puts function to print line of code
+ *
+ * Return: 0 (Signifying success)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
