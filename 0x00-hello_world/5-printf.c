e20634d015092da6a6f7ad873061967edff58e17
a-tornyie <a.tornyie@gmail.com>
2023-03-16 06:30:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Use printf function to print line of code
+ * Return: 0 (Signifies success)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
