8bd58a83e1d57639341e58dfc02bdabbfd0d22b4
a-tornyie <a.tornyie@gmail.com>
2023-03-17 08:48:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	while
+		(alphabet <= 'z') {
+			putchar(alphabet);
+			alphabet++;
+		}
+	putchar('\n');
+	return (0);
+}
