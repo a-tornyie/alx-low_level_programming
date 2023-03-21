33eede2ed843247f08b481abec9a1ce4dbf9496c
a-tornyie <a.tornyie@gmail.com>
2023-03-21 16:32:26 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,13 +12,13 @@ int main(void)
 	unsigned int n = 98;
 	unsigned int a = 1;
 	unsigned int b = 2;
+	unsigned int c;
 
 	printf("%u", a);
 	for (i = 1; i < n; i++)
 	{
 		printf(", %u", b);
-		unsigned int c = a + b;
-
+		c = a + b;
 		a = b;
 		b = c;
 	}
