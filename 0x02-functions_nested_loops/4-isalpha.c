380efb6bd3b6b8d9553efc35d2c283d0eee3f5da
a-tornyie <a.tornyie@gmail.com>
2023-03-21 08:12:35 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *__isalpha - Checks for a letter and
+ *_isalpha - Checks for a letter and
  * 1 if c is lowercase of uppercase and 0 otherwisw
  * @c: The character in ASCII code
  *
@@ -12,7 +12,7 @@ int _isalpha(int c)
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
-	}	
+	}
 	else
 	{
 		return (0);
