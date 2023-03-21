5f39e014a50b39653339aeaff6512269fd7ad4aa
a-tornyie <a.tornyie@gmail.com>
2023-03-21 10:47:03 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * 1 if c is lowercase and 0 otherwisw
+ * @a: input integer
+ * @b: input integer
+ *
+ * Return: a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
