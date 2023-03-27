0145ce0d8a7965582ffd89d8a0147e1ab94c12bf
a-tornyie <a.tornyie@gmail.com>
2023-03-27 03:08:55 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	b = (len / 2);
 	if ((len % 2) == 1)
 		b = ((len + 1) / 2);
-	for (a = b; str[a] != '\0' - ; a++)
+	for (a = b; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
