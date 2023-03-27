3618df63bbe277f70ccd570c38a8c9b93f6ac93a
a-tornyie <a.tornyie@gmail.com>
2023-03-27 02:46:57 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts2 - prints only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int b = 0;
+	char *a = str;
+	int i;
+
+	while (*a != '\n')
+	{
+		a++;
+		len++;
+	}
+
+	b = len - 1;
+
+	for (i = 0; i <= b; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
