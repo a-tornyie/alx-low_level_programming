4f4c8fe60a1e1938b61bf26e1e71aba164cdda10
a-tornyie <a.tornyie@gmail.com>
2023-07-06 14:22:31 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * converted_num + (b[i] - '0');
+	converted_num = 2 * converted_num + (b[i] - '0');
 	}
 
 	return (converted_num);
